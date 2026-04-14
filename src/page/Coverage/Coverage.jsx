b08fad2dec7a8e02ef8d7a59d6a05212cbf39963
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-04-14 09:11:43 +0600

    map page completed

--- src/page/Coverage/Coverage.jsx
+++ src/page/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
@@ -7,31 +7,42 @@ import { useLoaderData } from 'react-router';
 export default function Coverage() {
   const position = [23.6850, 90.3563];
   const serviceCenters = useLoaderData();
-  console.log(serviceCenters)
+  const mapRef = useRef(null);
+
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const locatin = e.target.location.value;
+    const district = serviceCenters.find(e => e.district.toLowerCase().includes(locatin.toLowerCase()));
+    if (district) {
+      const coord = [district.latitude, district.longitude]
+      mapRef.current.flyTo(coord, 15)
+
+    }
+
+  }
   return (
     <div className='card  p-5 '>
       <h1 className='text-5xl my-10 font-bold'>We are available in 64 districts</h1>
 
-      <div className="flex items-center p-4">
-        <div className="relative flex items-center w-[50%] max-w-2xl group">
-          {/* Search Icon */}
-          <div className="absolute left-6 text-gray-400 group-focus-within:text-[#00302d] transition-colors">
-            <FiSearch size={22} strokeWidth={3} />
-          </div>
+      <form onSubmit={handleSearch}>
+        <label className="input">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" className="grow" name='location' placeholder="Search" />
 
-          {/* Input Field */}
-          <input
-            type="text"
-            placeholder="Search here"
-            className="w-full bg-[#f0f3f7] py-4 pl-16 pr-32 rounded-full text-[#00302d] placeholder-gray-400 outline-none border-2 border-transparent focus:border-[#d4e9a9] transition-all"
-          />
-
-          {/* Search Button */}
-          <button className="absolute right-0 h-full bg-[#d4e9a9] text-[#00302d] font-bold px-10 rounded-full hover:bg-[#c5e1a5] transition-colors text-lg">
-            Search
-          </button>
-        </div>
-      </div>
+        </label>
+      </form>
       <div className='border h-[800px]'>
 
 
@@ -42,6 +53,7 @@ export default function Coverage() {
           scrollWheelZoom={false}
           position={position}
           className='h-full'
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
